846d344fd0be6b37baacf183c100c018a475b1c3
varun-chandran-11 <chandrasekharannairvarun@gmail.com>
2024-06-04 04:11:43 -0400

    Added test cases.

--- mobile-insight-service/src/test/java/com/varun/mobile/insight/service/impl/BillCycleUsageServiceImplTest.java
+++ mobile-insight-service/src/test/java/com/varun/mobile/insight/service/impl/BillCycleUsageServiceImplTest.java
@@ -6,16 +6,18 @@ import com.varun.mobile.insight.model.BillingCycle;
 import com.varun.mobile.insight.model.DailyUsage;
 import com.varun.mobile.insight.repository.BillingCycleRepository;
 import com.varun.mobile.insight.repository.DailyUsageRepository;
+import com.varun.mobile.insight.util.MIEncoder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -33,6 +35,11 @@ public class BillCycleUsageServiceImplTest {
     @InjectMocks
     private BillCycleUsageServiceImpl billingCycleUsageService;
 
+    private final String userId = "66595f7f832f0e6c0e31d75c";
+    private final String mdn = "4379892179";
+    private final int page = 0;
+    private final int size = 5;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
@@ -40,36 +47,47 @@ public class BillCycleUsageServiceImplTest {
 
     @Test
     public void testGetBillingCycleHistory_Success() throws BillingHistoryException {
-        List<BillingCycle> billingCycles = Arrays.asList(new BillingCycle(), new BillingCycle());
+        PageRequest pageRequest = PageRequest.of(page, size);
 
-        when(billingCycleRepository.findAll(anyString(), anyString())).thenReturn(billingCycles);
+        BillingCycle billingCycle1 = new BillingCycle();
+        billingCycle1.setUserId(userId);
+        billingCycle1.setMdn(mdn);
+        billingCycle1.setStartDate(new Date(System.currentTimeMillis() - 2000000));
+        billingCycle1.setEndDate(new Date(System.currentTimeMillis() - 1000000));
 
-        List<BillingCycle> result = billingCycleUsageService.getBillingCycleHistory("66595f7f832f0e6c0e31d75c", "4379892179");
+        BillingCycle billingCycle2 = new BillingCycle();
+        billingCycle2.setUserId(userId);
+        billingCycle2.setMdn(mdn);
+        billingCycle2.setStartDate(new Date(System.currentTimeMillis() - 1000000));
+        billingCycle2.setEndDate(new Date(System.currentTimeMillis() + 1000000));
+
+
+        List<BillingCycle> billingCycles = Arrays.asList(billingCycle1, billingCycle2);
+        Page<BillingCycle> billingCyclePage = new PageImpl<>(billingCycles);
+        when(billingCycleRepository.findAll(userId, MIEncoder.getInstance().encode(mdn), pageRequest)).thenReturn(billingCyclePage);
+
+        List<BillingCycle> result = billingCycleUsageService.getBillingCycleHistory(userId, mdn, page, size);
 
         assertNotNull(result);
-        assertEquals(2, result.size());
+        assertEquals(billingCyclePage.getContent(), result);
     }
 
     @Test
-    public void testGetCurrentCycleUsage_Success() throws CycleUsageException {
-        BillingCycle billingCycle = new BillingCycle();
-        billingCycle.setStartDate(new Date());
-        billingCycle.setEndDate(new Date());
-        List<DailyUsage> dailyUsages = Arrays.asList(new DailyUsage(), new DailyUsage());
+    void testGetBillingCycleHistoryThrowsException() {
 
-        when(billingCycleRepository.findItemByUserIdAndMdnAndDate(anyString(), anyString(), any(Date.class))).thenReturn(Optional.of(billingCycle));
-        when(dailyUsageRepository.findItemByUserIdAndMdnAndUsageDateBetween(anyString(), anyString(), any(Date.class), any(Date.class))).thenReturn(dailyUsages);
 
-        List<DailyUsage> result = billingCycleUsageService.getCurrentCycleUsage("66595f7f832f0e6c0e31d75c", "4379892179");
+        when(billingCycleRepository.findAll(userId, MIEncoder.getInstance().encode(mdn), PageRequest.of(page, size)))
+                .thenThrow(new DataAccessException("...") {});
 
-        assertNotNull(result);
-        assertEquals(2, result.size());
+        assertThrows(BillingHistoryException.class, () -> {
+            billingCycleUsageService.getBillingCycleHistory(userId, mdn, page, size);
+        });
     }
 
     @Test
     public void testGetCurrentCycleUsage_CycleUsageException() {
-        when(billingCycleRepository.findItemByUserIdAndMdnAndDate(anyString(), anyString(), any(Date.class))).thenReturn(Optional.empty());
 
-        assertThrows(CycleUsageException.class, () -> billingCycleUsageService.getCurrentCycleUsage("66595f7f832f0e6c0e31d75c", "4379892179"));
+        when(billingCycleRepository.findItemByUserIdAndMdnAndDate(anyString(), anyString(), any(Date.class))).thenReturn(Optional.empty());
+        assertThrows(CycleUsageException.class, () -> billingCycleUsageService.getCurrentCycleUsage(userId, mdn, page, size));
     }
 }
