c7a264977af2e84f14a43a7a7dbdc6a5a72b1499
varun-chandran-11 <chandrasekharannairvarun@gmail.com>
2024-06-04 03:43:30 -0400

    Removed @RequestBody from GetMapping as Open API 3.0 doesn't support it.

--- mobile-insight-service/src/main/java/com/varun/mobile/insight/controller/BillCycleUsageController.java
+++ mobile-insight-service/src/main/java/com/varun/mobile/insight/controller/BillCycleUsageController.java
@@ -8,9 +8,11 @@ import com.varun.mobile.insight.exception.CycleUsageException;
 import com.varun.mobile.insight.model.BillingCycle;
 import com.varun.mobile.insight.model.DailyUsage;
 import com.varun.mobile.insight.service.BillCycleUsageService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -30,7 +32,7 @@ public class BillCycleUsageController {
 
     @GetMapping("/history")
     @JsonView(Views.Public.class)
-    public ResponseEntity<List<BillingCycle>> getCycleHistory(@RequestBody BillingRequest request,
+    public ResponseEntity<List<BillingCycle>> getCycleHistory(BillingRequest request,
                                                               @RequestParam(defaultValue = "0") int page,
                                                               @RequestParam(defaultValue = "5") int size) throws BillingHistoryException {
         logger.log(Level.INFO, "Inside billing cycle controller history method.");
@@ -39,7 +41,7 @@ public class BillCycleUsageController {
 
     @GetMapping("/daily-usage")
     @JsonView(Views.Public.class)
-    public ResponseEntity<List<DailyUsage>> getCurrentCycleDailyUsage(@RequestBody BillingRequest request,
+    public ResponseEntity<List<DailyUsage>> getCurrentCycleDailyUsage(BillingRequest request,
                                                                       @RequestParam(defaultValue = "0") int page,
                                                                       @RequestParam(defaultValue = "5") int size) throws CycleUsageException {
         logger.log(Level.INFO, "Inside billing cycle controller daily usage method.");
