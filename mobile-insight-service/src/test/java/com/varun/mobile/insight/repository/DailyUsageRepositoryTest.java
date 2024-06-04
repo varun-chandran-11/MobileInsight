ac100ec448e09975f67203e187625edf3d484c87
varun-chandran-11 <chandrasekharannairvarun@gmail.com>
2024-06-04 02:31:36 -0400

    Updated test cases.

--- mobile-insight-service/src/test/java/com/varun/mobile/insight/repository/DailyUsageRepositoryTest.java
+++ mobile-insight-service/src/test/java/com/varun/mobile/insight/repository/DailyUsageRepositoryTest.java
@@ -1,50 +1,60 @@
 package com.varun.mobile.insight.repository;
 
-import com.varun.mobile.insight.model.UserDetail;
+import com.varun.mobile.insight.model.DailyUsage;
+import com.varun.mobile.insight.util.MIEncoder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
+import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 public class DailyUsageRepositoryTest {
 
     @Autowired
-    private UserDetailRepository userDetailRepository;
+    private DailyUsageRepository dailyUsageRepository;
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    private UserDetail userDetail;
+    private DailyUsage usage1;
+    private DailyUsage usage2;
+    private final String userId = "66595f7f832f0e6c0e31d75c";
+    private final String mdn = "4379892179";
 
     @BeforeEach
     public void setUp() {
-        userDetailRepository.deleteAll();
+       // dailyUsageRepository.deleteAll();
 
-        userDetail = new UserDetail();
-        userDetail.setFirstName("Varun");
-        userDetail.setLastName("Nair");
-        userDetail.setEmail("varun.chandran.11@gmail.com");
-        userDetail.setPassword("password");
+        usage1 = new DailyUsage();
+        usage1.setUserId(userId);
+        usage1.setMdn(mdn);
+        usage1.setUsedInMb(560.98);
+        usage1.setUsageDate(new Date(1622505600000L)); // 1st June 2021
+        //dailyUsageRepository.save(usage1);
 
-        mongoTemplate.save(userDetail);
+        usage2 = new DailyUsage();
+        usage2.setUserId(userId);
+        usage2.setMdn(mdn);
+        usage2.setUsedInMb(1023.87);
+        usage2.setUsageDate(new Date(1625097600000L)); // 1st July 2021
+        //dailyUsageRepository.save(usage2);
     }
 
     @Test
-    public void testFindById() {
-        Optional<UserDetail> result = userDetailRepository.findBy_id(userDetail.get_id());
+    public void testFindItemByUserIdAndMdnAndUsageDateBetween() {
 
-        assertTrue(result.isPresent());
-        assertEquals(userDetail.get_id(), result.get().get_id());
-        assertEquals("Varun", result.get().getFirstName());
-        assertEquals("Nair", result.get().getLastName());
-        assertEquals("varun.chandran.11@gmail.com", result.get().getEmail());
+        Pageable pageable = PageRequest.of(0, 10);
+        Date fromDate = new Date(1622419200000L); // 31st May 2021
+        Date toDate = new Date(1625184000000L); // 2nd July 2021
+
+        Page<DailyUsage> result = dailyUsageRepository.findItemByUserIdAndMdnAndUsageDateBetween(userId, MIEncoder.getInstance().encode(mdn), fromDate, toDate, pageable);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getContent()).hasSize(2);
     }
 
 }
